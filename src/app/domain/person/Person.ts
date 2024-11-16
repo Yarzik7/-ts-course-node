dba5e1e5072667a945206bc93d9c15d60117bb60
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-11-16 18:42:27 +0200

    Update Person.ts

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,5 +1,7 @@
 import { JsonController, Get, Post, Body, Param } from "routing-controllers";
 import { IPerson } from "./Person.types";
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
 
 const storageData: IPerson[] = [];
 
@@ -11,14 +13,22 @@ export default class Person {
   }
 
   @Get("/:id")
-  async getOne(@Param("id") id: number): Promise<IPerson | {}> {
+  async getOne(@Param("id") id: number): Promise<ApiResponse<IPerson | {}>> {
     const person = storageData.find((item) => item.id === id);
-    return person || {};
+
+    if (!person) {
+      throw new ApiError(404, {
+        code: "PERSON_NOT_FOUND",
+        message: `Person with id ${id} not found`,
+      });
+    }
+
+    return new ApiResponse(true, person);
   }
 
   @Post()
   async setPerson(@Body() body: IPerson) {
     storageData.push(body);
-    return true;
+    return new ApiResponse(true, "Person successfully created");
   }
 }
